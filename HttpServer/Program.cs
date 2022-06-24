fbd9982adc8f84639c22d429559ca4d66ad5b4c9
John Carlo Austria <johncarloaustria@protonmail.com>
2022-06-24 18:32:12 +0800

    Add project files.

---
+++ HttpServer/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write($"{Environment.NewLine}");
+Console.WriteLine(@"  HTTP Server");
+
+foreach (var arg in args)
+{
+    if (arg == null)
+    {
+
+    }
+
+    if (arg == "--help")
+    {
+        Console.WriteLine(@"  Usage: http-serve <options>");
+
+
+        Console.WriteLine($"{Environment.NewLine}");
+        Console.WriteLine(@"  Options:");
+        Console.WriteLine(@"    --help      - displays the help information for http-serve");
+        Console.WriteLine(@"    --port      - tell http-serve on which port to serve the index.html file");
+
+    }
+}
