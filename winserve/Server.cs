7bff1dc964cea9189b74b53f4128362855575ce8
John Carlo Austria <johncarloaustria@protonmail.com>
2022-07-05 22:46:55 +0800

    Add IDictionary

--- winserve/Server.cs
+++ winserve/Server.cs
@@ -2,6 +2,75 @@
 
 internal class Server
 {
+    // For loading css, js, etc.
+    private static IDictionary<string, string> mimeTypeMappings =
+        new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase)
+        {
+            #region extension to MIME type list
+            {   ".asf",     "video/x-ms-asf"                        },
+            {   ".asx",     "video/x-ms-asx"                        },
+            {   ".avi",     "video/x-msvideo"                       },
+            {   ".bin",     "application/octet-stream"              },
+            {   ".cco",     "application/cocoa"                     },
+            {   ".crt",     "application/x-x509-ca-cert"            },
+            {   ".css",     "text/css"                              },
+            {   ".deb",     "application/octet-stream"              },
+            {   ".der",     "application/x-x509-ca-cert"            },
+            {   ".dll",     "application/octet-stream"              },
+            {   ".dmg",     "application/octet-stream"              },
+            {   ".ear",     "application/java-archive"              },
+            {   ".eot",     "application/octet-stream"              },
+            {   ".exe",     "application/octet-stream"              },
+            {   ".flv",     "video/x-flv"                           },
+            {   ".gif",     "image/gif"                             },
+            {   ".hqx",     "applicaiton/mac-binhex40"              },
+            {   ".htc",     "text/x-component"                      },
+            {   ".htm",     "text/html"                             },
+            {   ".html",    "text/html"                             },
+            {   ".ico",     "image/x-icon"                          },
+            {   ".jar",     "application/java-archive"              },
+            {   ".jardiff", "application/x-java-archive-diff"       },
+            {   ".jng",     "image/x-jng"                           },
+            {   ".jnlp",    "application/x-java-jnlp-file"          },
+            {   ".jpeg",    "image/jpeg"                            },
+            {   ".jpg",     "image/jpeg"                            },
+            {   ".js",      "application/x-javascript"              },
+            {   ".mml",     "text/mathml"                           },
+            {   ".mng",     "video/x-mng"                           },
+            {   ".mov",     "video/quicktime"                       },
+            {   ".mp3",     "audio/mpeg"                            },
+            {   ".mpeg",    "video/mpeg"                            },
+            {   ".mpg",     "video/mpeg"                            },
+            {   ".msi",     "application/octet-stream"              },
+            {   ".msm",     "application/octet-stream"              },
+            {   ".msp",     "application/octet-stream"              },
+            {   ".pdb",     "application/x-pilot"                   },
+            {   ".pdf",     "application/pdf"                       },
+            {   ".pem",     "application/x-x509-ca-cert"            },
+            {   ".pl",      "application/x-perl"                    },
+            {   ".pm",      "application/x-perl"                    },
+            {   ".png",     "image/png"                             },
+            {   ".prc",     "application/x-pilot"                   },
+            {   ".ra",      "audio/x-realaudio"                     },
+            {   ".rar",     "application/x-rar-compressed"          },
+            {   ".rpm",     "application/x-redhat-package-manager"  },
+            {   ".rss",     "text/xml"                              },
+            {   ".run",     "application/x-makeself"                },
+            {   ".sea",     "application/x-sea"                     },
+            {   ".shtml",   "text/html"                             },
+            {   ".sit",     "application/x-stuffit"                 },
+            {   ".swf",     "application/x-shockwave-flash"         },
+            {   ".tcl",     "application/x-tcl"                     },
+            {   ".tk",      "application/x-tcl"                     },
+            {   ".txt",     "text/plain"                            },
+            {   ".war",     "application/java-archive"              },
+            {   ".wbmp",    "image/vnd.wap.wbmp"                    },
+            {   ".wmv",     "video/x-ms-wmv"                        },
+            {   ".xml",     "text/xml"                              },
+            {   ".xpi",     "application/x-xpinstall"               },
+            {   ".zip",     "application/zip"                       },
+            #endregion
+        };
     internal void Start(string[] prefixes)
     {
         HttpListener httpListener = new HttpListener();
@@ -60,6 +129,7 @@ internal class Server
             Console.WriteLine(e.Message);
         }
 
+        // This shouldn't return an empty string, I should really find a better way to do this
         return "";
     }
 }
