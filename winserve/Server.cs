ae335db136ec46f857606f26cbe51f69f06790a8
John Carlo Austria <johncarloaustria@protonmail.com>
2022-07-10 21:57:21 +0800

    Format Server.cs

--- winserve/Server.cs
+++ winserve/Server.cs
@@ -1,20 +1,20 @@
 ﻿using System.Net;
 
-    public class Server
-    {
+public class Server
+{
 
-        private static readonly string[] indexFiles =
-        {
+    private static readonly string[] indexFiles =
+    {
         "index.html",
         "index.htm",
         "default.html",
         "default.htm"
     };
 
-        // For loading css, js, etc.
-        private static IDictionary<string, string> mimeTypes =
-            new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase)
-            {
+    // For loading css, js, etc.
+    private static IDictionary<string, string> mimeTypes =
+        new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase)
+        {
             #region extension to MIME type list
             {   ".asf",     "video/x-ms-asf"                        },
             {   ".asx",     "video/x-ms-asx"                        },
@@ -79,132 +79,132 @@
             {   ".xml",     "text/xml"                              },
             {   ".xpi",     "application/x-xpinstall"               },
             {   ".zip",     "application/zip"                       },
-                #endregion
-            };
+            #endregion
+        };
 
-        private HttpListener? listener;
-        private Thread? thread;
-        private string path;
-        private string ip;
-        private int port;
+    private HttpListener? listener;
+    private Thread? thread;
+    private string path;
+    private string ip;
+    private int port;
 
-        public Server(int port, string path, string ip)
+    public Server(int port, string path, string ip)
+    {
+        this.path = path;
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public void Start()
+    {
+        if (thread != null) throw new Exception("winserve is already active. (Call stop first)");
+        thread = new Thread(Listen);
+        thread.Start();
+    }
+
+    private void Listen()
+    {
+        bool threadActive = true;
+
+        if (!HttpListener.IsSupported)
         {
-            this.path = path;
-            this.ip = ip;
-            this.port = port;
+            Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+            return;
         }
 
-        public void Start()
+        try
         {
-            if (thread != null) throw new Exception("winserve is already active. (Call stop first)");
-            thread = new Thread(Listen);
-            thread.Start();
+            listener = new HttpListener();
+            string prefix = string.Format("http://{0}:{1}/", ip, port);
+            listener.Prefixes.Add(prefix);
+            listener.Start();
+            Console.WriteLine("Listening...");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("ERROR:" + e.Message);
+            threadActive = false;
+            return;
         }
 
-        private void Listen()
-        {
-            bool threadActive = true;
 
-            if (!HttpListener.IsSupported)
-            {
-                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
-                return;
-            }
+        while (threadActive)
+        {
 
             try
             {
-                listener = new HttpListener();
-                string prefix = string.Format("http://{0}:{1}/", ip, port);
-                listener.Prefixes.Add(prefix);
-                listener.Start();
-                Console.WriteLine("Listening...");
+                // Note: The GetContext method blocks while waiting for a request.
+                HttpListenerContext context = listener.GetContext();
+                if (!threadActive) break;
+
+                ProcessContext(context);
+            }
+            catch (HttpListenerException ex)
+            {
+                Console.Write(ex);
+            }
+        }
+    }
+
+    private void ProcessContext(HttpListenerContext context)
+    {
+        string filename = context.Request.Url!.AbsolutePath;
+
+
+        if (filename is not null) filename = System.Web.HttpUtility.UrlDecode(filename.Substring(1));
+
+        if (string.IsNullOrEmpty(filename))
+        {
+            foreach (string indexFile in indexFiles)
+            {
+                if (File.Exists(Path.Combine(path, indexFile)))
+                {
+                    filename = indexFile;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine($"Serving file: {filename}");
+        filename = Path.Combine(path, filename!);
+
+        HttpStatusCode statusCode;
+
+        if (File.Exists(filename))
+        {
+            try
+            {
+                using (Stream stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    context.Response.ContentType = mimeTypes[Path.GetExtension(filename)];
+                    context.Response.ContentLength64 = stream.Length;
+
+                    // copy file stream to response
+                    stream.CopyTo(context.Response.OutputStream);
+                    stream.Flush();
+                    context.Response.OutputStream.Flush();
+                }
+
+                statusCode = HttpStatusCode.OK;
             }
             catch (Exception e)
             {
-                Console.WriteLine("ERROR:" + e.Message);
-                threadActive = false;
-                return;
-            }
-
-
-            while (threadActive)
-            {
-
-                try
-                {
-                    // Note: The GetContext method blocks while waiting for a request.
-                    HttpListenerContext context = listener.GetContext();
-                    if (!threadActive) break;
-
-                    ProcessContext(context);
-                }
-                catch (HttpListenerException ex)
-                {
-                    Console.Write(ex);
-                }
+                Console.WriteLine("ERROR: " + e.Message);
+                statusCode = HttpStatusCode.InternalServerError;
             }
         }
-
-        private void ProcessContext(HttpListenerContext context)
+        else
         {
-            string filename = context.Request.Url!.AbsolutePath;
-
-
-            if (filename is not null) filename = System.Web.HttpUtility.UrlDecode(filename.Substring(1));
-
-            if (string.IsNullOrEmpty(filename))
-            {
-                foreach (string indexFile in indexFiles)
-                {
-                    if (File.Exists(Path.Combine(path, indexFile)))
-                    {
-                        filename = indexFile;
-                        break;
-                    }
-                }
-            }
-
-            Console.WriteLine($"Serving file: {filename}");
-            filename = Path.Combine(path, filename!);
-
-            HttpStatusCode statusCode;
-
-            if (File.Exists(filename))
-            {
-                try
-                {
-                    using (Stream stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
-                    {
-                        context.Response.ContentType = mimeTypes[Path.GetExtension(filename)];
-                        context.Response.ContentLength64 = stream.Length;
-
-                        // copy file stream to response
-                        stream.CopyTo(context.Response.OutputStream);
-                        stream.Flush();
-                        context.Response.OutputStream.Flush();
-                    }
-
-                    statusCode = HttpStatusCode.OK;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("ERROR: " + e.Message);
-                    statusCode = HttpStatusCode.InternalServerError;
-                }
-            }
-            else
-            {
-                Console.WriteLine("File not found: " + filename);
-                statusCode = HttpStatusCode.NotFound;
-            }
-
-            context.Response.StatusCode = (int)statusCode;
-            if (statusCode == HttpStatusCode.OK)
-            {
-                context.Response.AddHeader("Date", DateTime.Now.ToString("R"));
-                context.Response.AddHeader("Last-Modified", File.GetLastWriteTime(filename).ToString("R"));
-            }
-            context.Response.OutputStream.Close();
+            Console.WriteLine("File not found: " + filename);
+            statusCode = HttpStatusCode.NotFound;
         }
+
+        context.Response.StatusCode = (int)statusCode;
+        if (statusCode == HttpStatusCode.OK)
+        {
+            context.Response.AddHeader("Date", DateTime.Now.ToString("R"));
+            context.Response.AddHeader("Last-Modified", File.GetLastWriteTime(filename).ToString("R"));
+        }
+        context.Response.OutputStream.Close();
     }
+}
